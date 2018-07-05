d3171337b13c9188ed49e5d8d01614df6877c00a
Ginger Dudley <gingerdudley@fb.com>
2018-07-05 16:44:16 -0700

    added tab bar to navigation controller

---
+++ twitter/View Controllers/ProfilePageViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfilePageViewController.h
+//  twitter
+//
+//  Created by Ginger Dudley on 7/5/18.
+//  Copyright © 2018 Emerson Malca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfilePageViewController : UIViewController
+
+@end
